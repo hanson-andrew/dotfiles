8d9afb2c6a2468f5b84247d789cc41ec155d6533
Andrew Hanson <andrew.hanson@carvana.com>
2024-03-06 13:53:04 -0700

    Updated install for p10k

--- home/ahanson/install.sh
+++ home/ahanson/install.sh
@@ -1,11 +1,12 @@
 dotfiles_dir=~/dotfiles
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rd ~/.p10k.zsh > /dev/null 2>&1
 
 ln -sf $dotfiles_dir/home/ahanson/.zshrc ~/.zshrc
+ln -sf $dotfiles_dir/home/ahanson/.p10k.zsh ~/.p10k.zsh
 
 sudo chsh -s /bin/zsh
 

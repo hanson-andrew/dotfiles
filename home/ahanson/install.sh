ac35d356ae86f46e45f6a7e568f2734ce464b345
Andrew Hanson <andrew.hanson@carvana.com>
2024-03-06 13:42:46 -0700

    More pathing updates

--- home/ahanson/install.sh
+++ home/ahanson/install.sh
@@ -1,5 +1,8 @@
 dotfiles_dir=~/dotfiles
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 
 ln -sf $dotfiles_dir/home/ahanson/.zshrc ~/.zshrc

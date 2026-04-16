69dee06ebcb630a0b510b3b65a715d1fa636cefe
Andrew Hanson <andrew.hanson@carvana.com>
2026-04-16 11:39:26 -0700

    Migrated to a purely nvim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,4 @@
+require("config.options")
+require("config.keymaps")
 require("config.lazy")
 
-vim.o.runtimepath = vim.o.runtimepath .. ",~/.vim,~/vim/after"
-vim.o.packpath = vim.o.runtimepath
-
-vim.cmd('source ~/.vimrc')

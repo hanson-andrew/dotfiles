722608d5e7788628a99669cf568aafd68e818aec
Andrew Hanson <andrew.hanson@carvana.com>
2026-05-05 08:53:32 -0700

    permissions change

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -14,6 +14,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 rm -rf /opt/nvim-linux-x86_64
 tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
-ln -sf /opt/nvim-linux-x86_:64/bin/nvim /opt/nvim/nvim
-
+sudo ln -sf /opt/nvim-linux-x86_:64/bin/nvim /opt/nvim/nvim
 

047d371e11b75df2de26921d020db98466567391
Andrew Hanson <andrew.hanson@carvana.com>
2026-05-05 08:58:11 -0700

    sudo all the things

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -11,7 +11,7 @@ sudo apt install -y "${PACKAGES[@]}"
 curl -fsSL https://raw.githubusercontent.com/devcontainers/cli/main/scripts/install.sh | sh
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-rm -rf /opt/nvim-linux-x86_64
+sudo rm -rf /opt/nvim-linux-x86_64
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
 sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /opt/nvim/nvim

9d96059dbcf4eb766efb7ba3289236a474a33aac
Andrew Hanson <andrew.hanson@carvana.com>
2026-05-05 08:57:02 -0700

    typo and another permission problem

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -12,7 +12,7 @@ curl -fsSL https://raw.githubusercontent.com/devcontainers/cli/main/scripts/inst
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
 rm -rf /opt/nvim-linux-x86_64
-tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
-sudo ln -sf /opt/nvim-linux-x86_:64/bin/nvim /opt/nvim/nvim
+sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /opt/nvim/nvim
 

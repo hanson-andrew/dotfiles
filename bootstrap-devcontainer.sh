83db8d8313c6073704c0ac69c1047a73b8f30c74
Andrew Hanson <andrew.hanson@carvana.com>
2026-05-07 07:06:39 -0700

    tmux plugin support

--- bootstrap-devcontainer.sh
+++ bootstrap-devcontainer.sh
@@ -17,3 +17,13 @@ rm nvim-linux-x86_64.tar.gz
 sudo mkdir -p /opt/nvim
 sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /opt/nvim/nvim
 
+TPM_DIR="${HOME}/.tmux/plugins/tpm"
+
+if [ ! -d "$TPM_DIR/.git" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+  git -C "$TPM_DIR" pull --ff-only
+fi
+
+"$TPM_DIR/bin/install_plugins"
+
--- bootstrap-ubuntu.sh
+++ bootstrap-ubuntu.sh
@@ -15,3 +15,13 @@ chmod u+x nvim-linux-x86_64.appimage
 sudo mkdir -p /opt/nvim
 sudo mv nvim-linux-x86_64.appimage /opt/nvim/nvim
 
+TPM_DIR="${HOME}/.tmux/plugins/tpm"
+
+if [ ! -d "$TPM_DIR/.git" ]; then
+  git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
+else
+  git -C "$TPM_DIR" pull --ff-only
+fi
+
+"$TPM_DIR/bin/install_plugins"
+
